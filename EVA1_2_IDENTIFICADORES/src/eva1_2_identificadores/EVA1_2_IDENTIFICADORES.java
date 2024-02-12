af98b06bfd4a0b3807d2ff44c59f969a2362f72b
Juan Ramon Ruiz <159494661+JuanRamonRuizAguilar@users.noreply.github.com>
2024-02-12 17:37:48 -0600

    Add files via upload

---
+++ EVA1_2_IDENTIFICADORES/src/eva1_2_identificadores/EVA1_2_IDENTIFICADORES.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_2_identificadores;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_2_IDENTIFICADORES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //VARIABLES:
+        //TIPO DE DATOS + NOMBRE DEL INDENTIFICADOR:
+        //Short int byte long --> ENTEROS
+        //float double --> numeros con decimal  
+        //char --> character --> caracteres
+        //boolean --> verdadero o falso
+        //String --> cadenas de texto
+        int edad; //decalaracion de una veriable
+        String nombre;
+        double salario;
+        //ASIGNACION DE VALORES:
+        //INDENTIFICADOR = VALORES;
+        //se coloca el valor 50 dentro de la variable edad
+        edad = 50;
+        nombre = "Juan Perez Jolote";
+        salario = 123.3;
+        //COMO LOS USAMOS:
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(salario);
+
+    }
+    
+}
---
+++ EVA1_5_ENTRADA_SALIDA/src/eva1_5_entrada_salida/EVA1_5_ENTRADA_SALIDA.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_5_entrada_salida;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_5_ENTRADA_SALIDA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Declaracion de variabales
+        String nombre;
+        String apellidos;
+        // Captura de variables
+        // Java reconoce mayusculas y minúsculas
+        System.out.println("Introduce tu nombre:");
+        System.out.println("apellidos");
+
+    }
+
+}
