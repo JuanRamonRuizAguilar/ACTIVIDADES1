55aa404b80d09053ac0f4c7faa948482f66a21c8
Juan Ramon Ruiz <159494661+JuanRamonRuizAguilar@users.noreply.github.com>
2024-04-16 21:36:08 -0600

    Add files via upload

---
+++ EVA2_18_FOR/src/main/java/com/mycompany/eva2_18_for/EVA2_18_FOR.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_18_for;
+
+/**
+ *
+ * @author juani
+ */
+public class EVA2_18_FOR {
+
+    public static void main(String[] args) {
+        for ( int i = 1; i<= 10; i++){
+            System.out.print(i+"-");
+        }
+        System.out.println("");
+            for(int i = 10; i >=1; i--){
+                System.out.print(i+"-");
+            }
+            for(int i = 0; i <= 100; i+=2){
+                System.out.println(i +"-");
+            }
+
+    }
+}
